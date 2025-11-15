55078f512f40459273673204602c96daddf7d1a0
ath331 <kstnftk@naver.com>
2025-11-15 15:58:35 +0900

    유니폼 사용과 무지개 삼각형

--- Test/main.cpp
+++ Test/main.cpp
@@ -5,16 +5,20 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 void framebuffer_size_callback( GLFWwindow* window, int width, int height )
@@ -124,10 +128,10 @@ int main()
 	glUseProgram( shaderProgram );
 
 	float vertices[] = {
-		0.5f, 0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f, 0.5f, 0.0f   // top left
+		// positions         // colors
+		0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // top 
 	};
 
 	unsigned int indices[] = {  // note that we start from 0!
@@ -142,28 +146,39 @@ int main()
 	glBindBuffer( GL_ARRAY_BUFFER, VBO );
 	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
 
-	unsigned int EBO;
-	glGenBuffers( 1, &EBO );
-	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
-	glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
-	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
-	glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+	//unsigned int EBO;
+	//glGenBuffers( 1, &EBO );
+	//glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+	//glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
+	//glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+	//glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
 
-	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof( float ), (void*)0 );
+	// position attribute
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), (void*)0 );
 	glEnableVertexAttribArray( 0 );
+	// color attribute
+	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), (void*)( 3 * sizeof( float ) ) );
+	glEnableVertexAttribArray( 1 );
 
 	glBindBuffer( GL_ARRAY_BUFFER, 0 );
 
-	glBindVertexArray( 0 );
+	//glBindVertexArray( 0 );
+
+	glUseProgram( shaderProgram );
 
 	while ( !glfwWindowShouldClose( window ) )
 	{
 		glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
 		glClear( GL_COLOR_BUFFER_BIT );
 
-		glUseProgram( shaderProgram );
+		float timeValue = glfwGetTime();
+		float greenValue = ( sin( timeValue ) / 2.0f ) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation( shaderProgram, "ourColor" );
+		glUniform4f( vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f );
+
 		glBindVertexArray( VAO );
-		glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+		//glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
+		glDrawArrays( GL_TRIANGLES, 0, 3 );
 
 		glfwSwapBuffers( window );
 		glfwPollEvents();
@@ -171,7 +186,7 @@ int main()
 
 	glDeleteVertexArrays( 1, &VAO );
 	glDeleteBuffers( 1, &VBO );
-	glDeleteBuffers( 1, &EBO );
+	//glDeleteBuffers( 1, &EBO );
 	glDeleteProgram( shaderProgram );
 
 	glfwTerminate();
